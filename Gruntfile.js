f5da5a8c10f039dfb75182dc4527352875ee188d
niwamaxou <niwamaxou@gmail.com>
2015-10-17 17:38:37 +0200

    * adding grunt tasks for distibution purposes

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,16 +3,35 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
 
-     // watch our node server for changes
+    pkg: grunt.file.readJSON('package.json'),
+
+    //get the current git revision
+     revision: {
+
+     },
+
+    // watch our node server for changes
     nodemon: {
       dev: {
         script: 'server.js',
       }
     },
 
+    //compress all the files needed to run
+    compress: {
+      main : {
+        options: {
+            archive: "dist/<%= pkg.name %>-<%= grunt.config.get('meta.revision') %>.zip"
+        },
+        files: [{src: ['node_modules/**', 'app/**', 'config/**', '*.js'], dest: 'dist'}]
+      }
+    },
   });
 
   grunt.loadNpmTasks('grunt-nodemon');
+  grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-git-revision');
 
   grunt.registerTask('default', ['nodemon']);
+  grunt.registerTask('dist', ['revision','compress']);
  };
