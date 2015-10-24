bcf16ecffd80c4a6b099bcb0e8e0112f51520fac
niwamaxou <niwamaxou@gmail.com>
2015-10-24 14:39:14 +0200

    * adding register route

--- app/routes.js
+++ app/routes.js
@@ -9,7 +9,8 @@ module.exports = function(app, express) {
     // middleware to use for all requests
     router.use(function(req, res, next) {
         // do logging
-        console.log(req.body);
+        if (req)
+            console.log(req.body);
         next(); // make sure we go to the next routes and don't stop here
     });
 
@@ -20,6 +21,8 @@ module.exports = function(app, express) {
    });
 
    // more routes for our API will happen here
+
+   //log the user
     router.route('/login')
         .post(function(req, res) {
             User.findOne(req.body.login, function (err, user) {
@@ -27,14 +30,13 @@ module.exports = function(app, express) {
                     res.status(401).json({error : 'bad login'});
                 else {
                     res.location('/users/' + user._id);
-                    res.status(201).send(null);
+                    res.status(200).send(null);
                 }
             });
         });
 
-
-    //create a new user and get them all
-    router.route('/users')
+    //register the user
+    router.route('/register')
         .post(function(req, res) {
             var user = new User();
             user.login = req.body.login;
@@ -42,13 +44,16 @@ module.exports = function(app, express) {
 
             user.save(function(err) {
                 if (err)
-                    res.send(err);
+                    res.status(500).json({error : "fail to register"});
                 else {
                     res.location('/users/' + user._id);
                     res.status(201).send(null);
                 }
             });
-        })
+        });
+
+    //get all users
+    router.route('/users')
         .get(function(req, res) {
             User.find(function(err, users) {
                 if (err)
@@ -58,6 +63,7 @@ module.exports = function(app, express) {
         });
 
 
+    //get one user from his id
     router.route('/users/:user_id')
         .get(function(req, res) {
             User.findById(req.params.user_id, function(err, user) {
